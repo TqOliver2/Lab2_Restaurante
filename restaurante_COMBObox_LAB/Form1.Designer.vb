02542bc7adf61f0b0395da832d6ff3baf205d411
Tonqadi <j.pixel.progaming@gmail.com>
2026-09-15 10:30:25 -0600

    Codigo basico agregado

--- restaurante_COMBObox_LAB/Form1.Designer.vb
+++ restaurante_COMBObox_LAB/Form1.Designer.vb
@@ -22,60 +22,65 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        ComboBox1 = New ComboBox()
-        ComboBox2 = New ComboBox()
-        ComboBox3 = New ComboBox()
+        cboARROCES = New ComboBox()
+        cboMENESTRAS = New ComboBox()
+        cboCARNES = New ComboBox()
         GroupBox1 = New GroupBox()
         Label3 = New Label()
         Label2 = New Label()
         Label1 = New Label()
         GroupBox2 = New GroupBox()
-        ListBox1 = New ListBox()
+        lstMENU = New ListBox()
         Label4 = New Label()
-        Button1 = New Button()
-        Button2 = New Button()
+        cmdBORRARMENU = New Button()
+        cmdFIN = New Button()
         Label5 = New Label()
-        TextBox1 = New TextBox()
+        txtTOTAL = New TextBox()
         Label6 = New Label()
         GroupBox1.SuspendLayout()
         GroupBox2.SuspendLayout()
         SuspendLayout()
         ' 
-        ' ComboBox1
+        ' cboARROCES
         ' 
-        ComboBox1.FormattingEnabled = True
-        ComboBox1.Location = New Point(6, 52)
-        ComboBox1.Name = "ComboBox1"
-        ComboBox1.Size = New Size(121, 23)
-        ComboBox1.TabIndex = 0
+        cboARROCES.FormattingEnabled = True
+        cboARROCES.Location = New Point(7, 69)
+        cboARROCES.Margin = New Padding(3, 4, 3, 4)
+        cboARROCES.Name = "cboARROCES"
+        cboARROCES.Size = New Size(138, 28)
+        cboARROCES.TabIndex = 0
         ' 
-        ' ComboBox2
+        ' cboMENESTRAS
         ' 
-        ComboBox2.FormattingEnabled = True
-        ComboBox2.Location = New Point(6, 116)
-        ComboBox2.Name = "ComboBox2"
-        ComboBox2.Size = New Size(121, 23)
-        ComboBox2.TabIndex = 1
+        cboMENESTRAS.FormattingEnabled = True
+        cboMENESTRAS.Location = New Point(7, 155)
+        cboMENESTRAS.Margin = New Padding(3, 4, 3, 4)
+        cboMENESTRAS.Name = "cboMENESTRAS"
+        cboMENESTRAS.Size = New Size(138, 28)
+        cboMENESTRAS.TabIndex = 1
         ' 
-        ' ComboBox3
+        ' cboCARNES
         ' 
-        ComboBox3.FormattingEnabled = True
-        ComboBox3.Location = New Point(6, 171)
-        ComboBox3.Name = "ComboBox3"
-        ComboBox3.Size = New Size(121, 23)
-        ComboBox3.TabIndex = 2
+        cboCARNES.FormattingEnabled = True
+        cboCARNES.Location = New Point(7, 228)
+        cboCARNES.Margin = New Padding(3, 4, 3, 4)
+        cboCARNES.Name = "cboCARNES"
+        cboCARNES.Size = New Size(138, 28)
+        cboCARNES.TabIndex = 2
         ' 
         ' GroupBox1
         ' 
         GroupBox1.Controls.Add(Label3)
         GroupBox1.Controls.Add(Label2)
-        GroupBox1.Controls.Add(ComboBox3)
+        GroupBox1.Controls.Add(cboCARNES)
         GroupBox1.Controls.Add(Label1)
-        GroupBox1.Controls.Add(ComboBox1)
-        GroupBox1.Controls.Add(ComboBox2)
-        GroupBox1.Location = New Point(47, 104)
+        GroupBox1.Controls.Add(cboARROCES)
+        GroupBox1.Controls.Add(cboMENESTRAS)
+        GroupBox1.Location = New Point(54, 139)
+        GroupBox1.Margin = New Padding(3, 4, 3, 4)
         GroupBox1.Name = "GroupBox1"
-        GroupBox1.Size = New Size(257, 215)
+        GroupBox1.Padding = New Padding(3, 4, 3, 4)
+        GroupBox1.Size = New Size(294, 287)
         GroupBox1.TabIndex = 3
         GroupBox1.TabStop = False
         GroupBox1.Text = "GroupBox1"
@@ -83,113 +88,121 @@ Partial Class Form1
         ' Label3
         ' 
         Label3.AutoSize = True
-        Label3.Location = New Point(11, 153)
+        Label3.Location = New Point(13, 204)
         Label3.Name = "Label3"
-        Label3.Size = New Size(51, 15)
+        Label3.Size = New Size(64, 20)
         Label3.TabIndex = 4
         Label3.Text = "CARNES"
         ' 
         ' Label2
         ' 
         Label2.AutoSize = True
-        Label2.Location = New Point(11, 93)
+        Label2.Location = New Point(13, 124)
         Label2.Name = "Label2"
-        Label2.Size = New Size(73, 15)
+        Label2.Size = New Size(92, 20)
         Label2.TabIndex = 3
         Label2.Text = "MENESTRAS"
         ' 
         ' Label1
         ' 
         Label1.AutoSize = True
-        Label1.Location = New Point(6, 34)
+        Label1.Location = New Point(7, 45)
         Label1.Name = "Label1"
-        Label1.Size = New Size(58, 15)
+        Label1.Size = New Size(73, 20)
         Label1.TabIndex = 2
         Label1.Text = "ARROCES"
         ' 
         ' GroupBox2
         ' 
-        GroupBox2.Controls.Add(ListBox1)
+        GroupBox2.Controls.Add(lstMENU)
         GroupBox2.Controls.Add(Label4)
-        GroupBox2.Location = New Point(426, 112)
+        GroupBox2.Location = New Point(487, 149)
+        GroupBox2.Margin = New Padding(3, 4, 3, 4)
         GroupBox2.Name = "GroupBox2"
-        GroupBox2.Size = New Size(319, 207)
+        GroupBox2.Padding = New Padding(3, 4, 3, 4)
+        GroupBox2.Size = New Size(365, 276)
         GroupBox2.TabIndex = 4
         GroupBox2.TabStop = False
         GroupBox2.Text = "GroupBox2"
         ' 
-        ' ListBox1
+        ' lstMENU
         ' 
-        ListBox1.FormattingEnabled = True
-        ListBox1.Location = New Point(26, 54)
-        ListBox1.Name = "ListBox1"
-        ListBox1.Size = New Size(275, 139)
-        ListBox1.TabIndex = 2
+        lstMENU.FormattingEnabled = True
+        lstMENU.Location = New Point(30, 72)
+        lstMENU.Margin = New Padding(3, 4, 3, 4)
+        lstMENU.Name = "lstMENU"
+        lstMENU.Size = New Size(314, 184)
+        lstMENU.TabIndex = 2
         ' 
         ' Label4
         ' 
         Label4.AutoSize = True
-        Label4.Location = New Point(16, 26)
+        Label4.Location = New Point(18, 35)
         Label4.Name = "Label4"
-        Label4.Size = New Size(130, 15)
+        Label4.Size = New Size(162, 20)
         Label4.TabIndex = 0
         Label4.Text = "MENU SELECCIONADO"
         ' 
-        ' Button1
+        ' cmdBORRARMENU
         ' 
-        Button1.Location = New Point(69, 355)
-        Button1.Name = "Button1"
-        Button1.Size = New Size(115, 29)
-        Button1.TabIndex = 5
-        Button1.Text = "BORRAR MENU"
-        Button1.UseVisualStyleBackColor = True
+        cmdBORRARMENU.Location = New Point(79, 473)
+        cmdBORRARMENU.Margin = New Padding(3, 4, 3, 4)
+        cmdBORRARMENU.Name = "cmdBORRARMENU"
+        cmdBORRARMENU.Size = New Size(131, 39)
+        cmdBORRARMENU.TabIndex = 5
+        cmdBORRARMENU.Text = "BORRAR MENU"
+        cmdBORRARMENU.UseVisualStyleBackColor = True
         ' 
-        ' Button2
+        ' cmdFIN
         ' 
-        Button2.Location = New Point(69, 402)
-        Button2.Name = "Button2"
-        Button2.Size = New Size(115, 23)
-        Button2.TabIndex = 6
-        Button2.Text = "FINALIZAR"
-        Button2.UseVisualStyleBackColor = True
+        cmdFIN.Location = New Point(79, 536)
+        cmdFIN.Margin = New Padding(3, 4, 3, 4)
+        cmdFIN.Name = "cmdFIN"
+        cmdFIN.Size = New Size(131, 31)
+        cmdFIN.TabIndex = 6
+        cmdFIN.Text = "FINALIZAR"
+        cmdFIN.UseVisualStyleBackColor = True
         ' 
         ' Label5
         ' 
         Label5.AutoSize = True
-        Label5.Location = New Point(537, 341)
+        Label5.Location = New Point(614, 455)
         Label5.Name = "Label5"
-        Label5.Size = New Size(106, 15)
+        Label5.Size = New Size(132, 20)
         Label5.TabIndex = 7
         Label5.Text = "TOTAL DE CUENTA"
         ' 
-        ' TextBox1
+        ' txtTOTAL
         ' 
-        TextBox1.Location = New Point(519, 377)
-        TextBox1.Name = "TextBox1"
-        TextBox1.Size = New Size(143, 23)
-        TextBox1.TabIndex = 8
+        txtTOTAL.Location = New Point(593, 503)
+        txtTOTAL.Margin = New Padding(3, 4, 3, 4)
+        txtTOTAL.Name = "txtTOTAL"
+        txtTOTAL.ReadOnly = True
+        txtTOTAL.Size = New Size(163, 27)
+        txtTOTAL.TabIndex = 8
         ' 
         ' Label6
         ' 
         Label6.AutoSize = True
-        Label6.Location = New Point(217, 38)
+        Label6.Location = New Point(248, 51)
         Label6.Name = "Label6"
-        Label6.Size = New Size(85, 15)
+        Label6.Size = New Size(107, 20)
         Label6.TabIndex = 9
         Label6.Text = "RESTAURANTE"
         ' 
         ' Form1
         ' 
-        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(800, 450)
+        ClientSize = New Size(914, 600)
         Controls.Add(Label6)
-        Controls.Add(TextBox1)
+        Controls.Add(txtTOTAL)
         Controls.Add(Label5)
-        Controls.Add(Button2)
-        Controls.Add(Button1)
+        Controls.Add(cmdFIN)
+        Controls.Add(cmdBORRARMENU)
         Controls.Add(GroupBox2)
         Controls.Add(GroupBox1)
+        Margin = New Padding(3, 4, 3, 4)
         Name = "Form1"
         Text = "Form1"
         GroupBox1.ResumeLayout(False)
@@ -200,20 +213,20 @@ Partial Class Form1
         PerformLayout()
     End Sub
 
-    Friend WithEvents ComboBox1 As ComboBox
-    Friend WithEvents ComboBox2 As ComboBox
-    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents cboARROCES As ComboBox
+    Friend WithEvents cboMENESTRAS As ComboBox
+    Friend WithEvents cboCARNES As ComboBox
     Friend WithEvents GroupBox1 As GroupBox
     Friend WithEvents Label1 As Label
     Friend WithEvents Label3 As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents GroupBox2 As GroupBox
     Friend WithEvents Label4 As Label
-    Friend WithEvents ListBox1 As ListBox
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
+    Friend WithEvents lstMENU As ListBox
+    Friend WithEvents cmdBORRARMENU As Button
+    Friend WithEvents cmdFIN As Button
     Friend WithEvents Label5 As Label
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents txtTOTAL As TextBox
     Friend WithEvents Label6 As Label
 
 End Class
--- restaurante_COMBObox_LAB/Form1.vb
+++ restaurante_COMBObox_LAB/Form1.vb
@@ -1,4 +1,143 @@
 ﻿Public Class Form1
 
+    Private Sub cmdFIN_Click(sender As Object, e As EventArgs) Handles cmdFIN.Click
+        End
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        'USANDO EL METODO ADD
+        cboMENESTRAS.Items.Add("POROTOS: 1.15")
+        cboMENESTRAS.Items.Add("LENTEJAS: 0.90")
+        cboMENESTRAS.Items.Add("FRIJOLES: 1.00")
+        cboMENESTRAS.Items.Add("ARVEJAS: 0.95")
+        cboMENESTRAS.Items.Add("GARBANZOS: 0.85")
+
+        'USANDO EL METODO DE VECTORES ADDRANGE
+        Dim LASCARNES(6) As String
+
+        LASCARNES(0) = "CARNES DE RES: 1.55"
+        LASCARNES(1) = "VENADO: 2.50"
+        LASCARNES(2) = "POLLO: 1.15"
+        LASCARNES(3) = "PAVO: 2.15"
+        LASCARNES(4) = "PESCADO: 2.50"
+        LASCARNES(5) = "CAMARONES: 2.90"
+        LASCARNES(6) = "LANGOSTA: 3.50"
+
+        'ASIGNANDO LOS VECTORES AL COMBOBOX
+        cboCARNES.Items.AddRange(LASCARNES)
+
+        ' ASIGNANDO LOS ARROCES
+        cboARROCES.Items.Add("ARROZ BLANCO: 1.00")
+        cboARROCES.Items.Add("ARROZ CON VEGETALES: 1.15")
+        cboARROCES.Items.Add("ARROZ CON MAÍZ: 1.10")
+        cboARROCES.Items.Add("ARROZ CON FRIJOLES: 1.20")
+        cboARROCES.Items.Add("ARROZ FRITO: 1.50")
+
+
+    End Sub
+
+    Private Sub cboARROCES_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboARROCES.SelectedIndexChanged
+
+        Dim PRECIOARROZ, TOTAL As Single
+        Dim POSICION As Integer
+
+        'AGREGAMOS EL ARROZ SELECCIONADO
+        'EN CBOARROZ AL LISTMENU
+        lstMENU.Items.Add(cboARROCES.Text)
+
+        'LEEMOS Y FIJAMOS LA POSICION DE LOS (:)
+        POSICION = InStr(cboARROCES.Text, ":")
+
+        'LEEMOS Y TRANSFORMAMOS Y CAPTURAMOS TODOS LOS CARACTERES DESPUES DE (:)
+        PRECIOARROZ = Val(Mid(cboARROCES.Text, POSICION + 1))
+
+        'SUMAMOS AL TOTAL EL PRECIO DEL ARROZ Y LE DAMOS FORMATO
+        TOTAL = Val(txtTOTAL.Text) + PRECIOARROZ
+        txtTOTAL.Text = Format(TOTAL, "##0.00")
+
+    End Sub
+
+    Private Sub cboMENESTRAS_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboMENESTRAS.SelectedIndexChanged
+
+        Dim PRECIOMENESTRA, TOTAL As Single
+        Dim POSICION As Integer
+
+        'AGREGAMOS EL MENESTRA SELECCIONADO
+        'EN CBOMENESTRAS AL LISTMENU
+        lstMENU.Items.Add(cboMENESTRAS.Text)
+
+        'LEEMOS Y FIJAMOS LA POSICION DE LOS (:)
+        POSICION = InStr(cboMENESTRAS.Text, ":")
+
+        'LEEMOS Y TRANSFORMAMOS Y CAPTURAMOS TODOS LOS CARACTERES DESPUES DE (:)
+        PRECIOMENESTRA = Val(Mid(cboMENESTRAS.Text, POSICION + 1))
+
+        'SUMAMOS AL TOTAL EL PRECIO DEL MENESTRA Y LE DAMOS FORMATO
+        TOTAL = Val(txtTOTAL.Text) + PRECIOMENESTRA
+        txtTOTAL.Text = Format(TOTAL, "##0.00")
+
+    End Sub
+
+    Private Sub cboCARNES_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboCARNES.SelectedIndexChanged
+
+        Dim PRECIOCARNES, TOTAL As Single
+        Dim POSICION As Integer
+
+        'AGREGAMOS EL CARNES SELECCIONADO
+        'EN CBOCARNES AL LISTMENU
+        lstMENU.Items.Add(cboCARNES.Text)
+
+        'LEEMOS Y FIJAMOS LA POSICION DE LOS (:)
+        POSICION = InStr(cboCARNES.Text, ":")
+
+        'LEEMOS Y TRANSFORMAMOS Y CAPTURAMOS TODOS LOS CARACTERES DESPUES DE (:)
+        PRECIOCARNES = Val(Mid(cboCARNES.Text, POSICION + 1))
+
+        'SUMAMOS AL TOTAL EL PRECIO DEL CARNES Y LE DAMOS FORMATO
+        TOTAL = Val(txtTOTAL.Text) + PRECIOCARNES
+        txtTOTAL.Text = Format(TOTAL, "##0.00")
+
+    End Sub
+
+    Private Sub lstMENU_DoubleClick(sender As Object, e As EventArgs) Handles lstMENU.DoubleClick
+        Dim PRECIOELIMINAR, TOTAL As Single
+        Dim POSICION As Integer
+
+        'POSICION DE LOS (:)
+        POSICION = InStr(lstMENU.Text, ":")
+
+        'LEEMOS Y TRANSFORMAMOS Y CAPTURAMOS TODOS LOS CARACTERES DESPUES DE (:)
+        PRECIOELIMINAR = Val(Mid(lstMENU.Text, POSICION + 1))
+
+        'VERIFICAMOS VALORES EN LA LISTA Y LUEGO RESTAMOS
+        If lstMENU.Items.Count > 1 Then
+            TOTAL = Val(txtTOTAL.Text) - PRECIOELIMINAR
+            txtTOTAL.Text = Format(TOTAL, "##0.00")
+        ElseIf lstMENU.Items.Count = 1 Then
+            txtTOTAL.Text = ""
+        Else
+            Exit Sub
+        End If
+
+        'REMOVEMOS EL ELEMENTO AL QUE LE DIMOS DOBLE CLIC
+        lstMENU.Items.Remove(lstMENU.Text)
+    End Sub
+
+    Private Sub cmdBORRARMENU_Click(sender As Object, e As EventArgs) Handles cmdBORRARMENU.Click
+        'BORRANDO COMBOS
+        cboARROCES.Text = ""
+        cboMENESTRAS.Text = ""
+        cboCARNES.Text = ""
+
+        'BORRANDO LISTA
+        lstMENU.Items.Clear()
+
+        'BORRANDO TOTAL
+        txtTOTAL.Clear()
+    End Sub
+
+
 
 End Class
+
